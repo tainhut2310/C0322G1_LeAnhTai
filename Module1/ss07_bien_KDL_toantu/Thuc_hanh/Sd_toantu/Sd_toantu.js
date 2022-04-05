b3575f14930bd661bc9965cba7a3a71d30059993
TaiNhut231094 <anhtai231094@gmail.com>
2022-04-05 20:22:32 +0700

    update bai tap

---
+++ Module1/ss07_bien_KDL_toantu/Thuc_hanh/Sd_toantu/Sd_toantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth= prompt('Moi ban nhap chieu cao');
+inputHeight=prompt('Moi ban nhap chieu rong');
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+let area =width * height
+document.write(" The area is:" + area);
