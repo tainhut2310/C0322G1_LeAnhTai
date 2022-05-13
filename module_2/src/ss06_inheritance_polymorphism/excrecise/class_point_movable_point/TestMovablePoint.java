f491d8f99b9ef640a0e39d07a270a2f701d90917
TaiNhut231094 <anhtai231094@gmail.com>
2022-05-13 15:19:13 +0700

    update

--- module_2/src/ss06_inheritance_polymorphism/excrecise/class_point_movable_point/TestMovablePoint.java
+++ module_2/src/ss06_inheritance_polymorphism/excrecise/class_point_movable_point/TestMovablePoint.java
@@ -2,12 +2,13 @@ package ss06_inheritance_polymorphism.excrecise.class_point_movable_point;
 
 public class TestMovablePoint {
     public static void main(String[] args) {
-        MovablePoint movablePoint = new MovablePoint(3.3f, 7.7f);
-        System.out.println(movablePoint.toString());
+        MovablePoint movablePoint = new MovablePoint();
         movablePoint.setSpeed(2.2f, 5.5f);
-        System.out.println(movablePoint.move());
+        System.out.println(movablePoint);
         movablePoint.setX(2.2f);
         movablePoint.setY(3.3f);
+        System.out.println(movablePoint.move());
+        System.out.println(movablePoint.move());
         System.out.println(movablePoint.move());
     }
 }
