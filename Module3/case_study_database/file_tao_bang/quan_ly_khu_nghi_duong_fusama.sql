9ea5142afa462f343dea64cce966d55fbb9d3afb
TaiNhut231094 <anhtai231094@gmail.com>
2022-06-16 23:14:31 +0700

    update casestudy

--- Module3/case_study_database/file_tao_bang/quan_ly_khu_nghi_duong_fusama.sql
+++ Module3/case_study_database/file_tao_bang/quan_ly_khu_nghi_duong_fusama.sql
@@ -24,7 +24,6 @@ dia_chi varchar(45),
 ma_vi_tri int not null,
 ma_trinh_do int not null,
 ma_bo_phan int not null,
-unique (ma_vi_tri, ma_trinh_do, ma_bo_phan),
 foreign key (ma_vi_tri) references vi_tri (ma_vi_tri),
 foreign key (ma_trinh_do) references trinh_do (ma_trinh_do),
 foreign key (ma_bo_phan) references bo_phan (ma_bo_phan)
@@ -35,15 +34,15 @@ ten_loai_khach varchar(45) not null
 );
 create table khach_hang(
 ma_khach_hang int auto_increment primary key,
-ma_loai_khach int not null,
-foreign key (ma_loai_khach) references loai_khach (ma_loai_khach),
 ho_ten varchar(45) not null,
 ngay_sinh date,
 gioi_tinh bit(1),
 so_cmnd varchar(45),
 so_dien_thoai varchar(45),
 email varchar(45),
-dia_chi varchar(45)
+dia_chi varchar(45),
+ma_loai_khach int not null,
+foreign key (ma_loai_khach) references loai_khach (ma_loai_khach)
 );
 create table kieu_thue(
 ma_kieu_thue int auto_increment primary key,
@@ -58,15 +57,15 @@ ma_dich_vu int auto_increment primary key,
 ten_dich_vu varchar(45) not null,
 dien_tich int not null,
 chi_phi_thue double not null,
-ma_kieu_thue int not null,
-ma_loai_dich_vu int not null,
-unique (ma_kieu_thue, ma_loai_dich_vu),
-foreign key (ma_kieu_thue) references kieu_thue (ma_kieu_thue),
-foreign key (ma_loai_dich_vu) references loai_dich_vu (ma_loai_dich_vu),
+so_nguoi_toi_da int not null,
 tieu_chuan_phong varchar(45) not null,
 mo_ta_tien_nghi_khac varchar(45),
-dien_tich_ho_boi double not null,
-so_tang int not null
+dien_tich_ho_boi double,
+so_tang int,
+ma_kieu_thue int not null,
+ma_loai_dich_vu int not null,
+foreign key (ma_kieu_thue) references kieu_thue (ma_kieu_thue),
+foreign key (ma_loai_dich_vu) references loai_dich_vu (ma_loai_dich_vu)
 );
 create table hop_dong(
 ma_hop_dong int auto_increment primary key,
@@ -76,7 +75,6 @@ tien_dat_coc double not null,
 ma_nhan_vien int not null,
 ma_khach_hang int not null,
 ma_dich_vu int not null,
-unique (ma_nhan_vien, ma_khach_hang, ma_dich_vu),
 foreign key (ma_nhan_vien) references nhan_vien (ma_nhan_vien),
 foreign key (ma_khach_hang) references khach_hang (ma_khach_hang),
 foreign key (ma_dich_vu) references dich_vu (ma_dich_vu)
@@ -85,15 +83,16 @@ create table dich_vu_di_kem (
 ma_dich_vu_di_kem int auto_increment primary key,
 ten_dich_vu_di_kem varchar(45),
 gia double not null,
+don_vi varchar(10),
 trang_thai varchar(45)
 );
 create table hop_dong_chi_tiet(
 ma_hop_dong_chi_tiet int auto_increment primary key,
+so_luong int,
 ma_hop_dong int not null,
 ma_dich_vu_di_kem int not null,
 foreign key (ma_hop_dong) references hop_dong (ma_hop_dong),
-foreign key (ma_dich_vu_di_kem) references dich_vu_di_kem (ma_dich_vu_di_kem),
-so_luong int
+foreign key (ma_dich_vu_di_kem) references dich_vu_di_kem (ma_dich_vu_di_kem)
 );
 
 
