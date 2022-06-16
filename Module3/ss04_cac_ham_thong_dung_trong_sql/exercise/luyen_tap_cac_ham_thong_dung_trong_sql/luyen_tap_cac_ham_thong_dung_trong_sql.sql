27960e9a832cd88ea1b9dbfec002df6f94452b35
TaiNhut231094 <anhtai231094@gmail.com>
2022-06-16 15:30:53 +0700

    edit exercise

--- Module3/ss04_cac_ham_thong_dung_trong_sql/exercise/luyen_tap_cac_ham_thong_dung_trong_sql/luyen_tap_cac_ham_thong_dung_trong_sql.sql
+++ Module3/ss04_cac_ham_thong_dung_trong_sql/exercise/luyen_tap_cac_ham_thong_dung_trong_sql/luyen_tap_cac_ham_thong_dung_trong_sql.sql
@@ -1,8 +1,10 @@
 use quan_ly_sinh_vien;
-select `subject`.*, max(`subject`.credit) as max_credit from `subject`;
+select `subject`.* from `subject`
+where credit in (select max(`subject`.credit) as max_credit from `subject`);
 
-select `subject`.*, max(mark.mark) as max_mark from `subject`
-join mark on `subject`.sub_id = mark.sub_id;
+select `subject`.* from `subject`
+join mark on `subject`.sub_id = mark.sub_id
+where mark in (select max(mark.mark) from mark);
 
 select student.*, avg(mark.mark) as DTB from student 
 join mark on student.student_id = mark.student_id
