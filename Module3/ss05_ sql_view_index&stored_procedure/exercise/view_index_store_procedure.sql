347952ee87dbac70f1f03048a2158333e82a533d
TaiNhut231094 <anhtai231094@gmail.com>
2022-06-17 16:19:23 +0700

    exit exercise

--- Module3/ss05_ sql_view_index&stored_procedure/exercise/view_index_store_procedure.sql
+++ Module3/ss05_ sql_view_index&stored_procedure/exercise/view_index_store_procedure.sql
@@ -12,11 +12,11 @@ product_status varchar(45)
 );
 -- Bước 2: Tạo bảng Products với các trường dữ liệu sau:
 insert into products (product_code, product_name, product_price, product_amount, product_description, product_status) 
-value ('I123','iPhone 10', 10000000, 50, 'Gold', 'Ngừng sản xuất'), 
- ('I456', 'iPhone 11', 12000000, 100, 'Green', 'Còn hàng'), 
- ('S123', 'Samsung A10', 7000000, 150, 'Red', 'Còn hàng'), 
- ('N123', 'Nokia N10', 6000000, 200, 'Black', 'Ngừng sản xuất'), 
- ('I678', 'iPhone 12', 15000000, 180, 'White', 'Còn hàng');
+value ('I123','iPhone 10', 10000000, 50, 'Hàng nguyên zin', 'Ngừng sản xuất'), 
+ ('I456', 'iPhone 11', 12000000, 100, 'Hàng 99%', 'Còn hàng'), 
+ ('S123', 'Samsung A10', 7000000, 150, 'Hàng xách tay', 'Còn hàng'), 
+ ('N123', 'Nokia N10', 6000000, 200, 'Hàng mới 100%', 'Ngừng sản xuất'), 
+ ('I678', 'iPhone 12', 15000000, 180, 'Hàng cũ', 'Còn hàng');
  -- Bước 3:
  select * from products where product_code = 'S123'; 
  
@@ -62,24 +62,34 @@ call get_information();
 
 -- Tạo store procedure thêm một sản phẩm mới
 delimiter //
-create procedure add_new_product ()
+create procedure add_new_product (id int, product_code varchar(20),
+product_name varchar(20),
+product_price double,
+product_amount int,
+product_description varchar(45),
+product_status varchar(45))
 begin 
-insert into products (product_code, product_name, product_price, product_amount, product_description, product_status)
-value ('I1314', 'iPhone 13', 25000000, 100, 'Gold', 'Còn hàng');
+insert into products 
+value (id, product_code, product_name, product_price, product_amount, product_description, product_status);
 end //
 delimiter ; 
-call add_new_product();
+call add_new_product(6, 'I1314', 'iPhone 13', 25000000, 100, 'Hàng chính hãng', 'Còn hàng');
   
 --   Tạo store procedure sửa thông tin sản phẩm theo id
   delimiter //
-create procedure edit_product (in key_name int)
+create procedure edit_product (in key_name int, product_code1 varchar(20),
+product_name1 varchar(20),
+product_price1 double,
+product_amount1 int,
+product_description1 varchar(45),
+product_status1 varchar(45))
 begin 
-update products set product_code = 'S1080', product_name =  'Samsung A3', 
-product_price = 5000000, product_amount = 50, product_description = 'Green', product_status = 'Còn hàng'
+update products set product_code = product_code1,  product_name = product_name1 , 
+product_price = product_price1, product_amount = product_amount1, product_description = product_description1 , product_status = product_status1
 where id = key_name;
 end //
 delimiter ; 
-call edit_product(6);
+call edit_product(6, 'S1080', 'Samsung A3', 5000000, 50, 'Green', 'Còn hàng');
 
 -- Tạo store procedure xoá sản phẩm theo id
 delimiter //
