9501da1713d0cfdb0a78a4314845eb86858fef1e
TaiNhut231094 <anhtai231094@gmail.com>
2022-06-27 16:38:41 +0700

    update exercise

---
+++ Module3/ss09_tong_quan_ung_dung_web/exercise/ung_dung_product_discount_caclucator/src/main/java/DiscountServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "DiscountServlet", urlPatterns = "/discount")
+public class DiscountServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float listPrice = Float.parseFloat(request.getParameter("numberListPrice"));
+        float discountPercent = Float.parseFloat(request.getParameter("numberDiscountRate"));
+        double discountAmount = listPrice * discountPercent * 0.01;
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Discount Amount: " + discountAmount + "</h1>");
+        writer.println("<h1>Discount Price: " + (listPrice - discountAmount) + "</h1>");
+        writer.println("</html>");
+    }
+}
