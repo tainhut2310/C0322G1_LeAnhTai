1f6a25035daf28bf48f8106159947fd953628bf9
TaiNhut231094 <anhtai231094@gmail.com>
2022-07-13 16:13:50 +0700

    edit exercise

--- Module3/ss01_co_so_du_lieu_quan_he/xay_dung_co_so_du_lieu_quan_ly_sinh_vien/xay_dung_co_so_du_lieu_quan_ly_sinh_vien.sql
+++ Module3/ss01_co_so_du_lieu_quan_he/xay_dung_co_so_du_lieu_quan_ly_sinh_vien/xay_dung_co_so_du_lieu_quan_ly_sinh_vien.sql
@@ -3,12 +3,12 @@ create database `student-management`;
 use `student-management`;
 
 CREATE TABLE class (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT,
     `name` VARCHAR(50)
 );	
 
 CREATE TABLE teacher (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT,
     `name` VARCHAR(50),
     age INT NOT NULL,
     country VARCHAR(50) NOT NULL
