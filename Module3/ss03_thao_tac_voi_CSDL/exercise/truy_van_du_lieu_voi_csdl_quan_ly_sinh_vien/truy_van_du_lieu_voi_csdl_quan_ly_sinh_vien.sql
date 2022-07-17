f07b4539e009325faa46ef78b47111192eaff5f4
TaiNhut231094 <anhtai231094@gmail.com>
2022-07-18 06:48:24 +0700

    edit exercise

--- Module3/ss03_thao_tac_voi_CSDL/exercise/truy_van_du_lieu_voi_csdl_quan_ly_sinh_vien/truy_van_du_lieu_voi_csdl_quan_ly_sinh_vien.sql
+++ Module3/ss03_thao_tac_voi_CSDL/exercise/truy_van_du_lieu_voi_csdl_quan_ly_sinh_vien/truy_van_du_lieu_voi_csdl_quan_ly_sinh_vien.sql
@@ -3,52 +3,6 @@ USE quan_ly_sinh_vien;
 SELECT 
     *
 FROM
-    student;   
-    
-SELECT 
-    *
-FROM
-    student
-WHERE
-    status = TRUE;
-
-SELECT 
-    *
-FROM
-    `subject`
-WHERE
-    credit < 10;
-
-SELECT 
-    s.student_id, s.student_name, c.class_name
-FROM
-    student s
-        JOIN
-    class c ON s.class_id = c.class_id;
-
-SELECT 
-    s.student_id, s.student_name, c.class_name
-FROM
-    student s
-        JOIN
-    class c ON s.class_id = c.class_id
-WHERE
-    c.class_name = 'A1';
-
-SELECT 
-    s.student_id, s.student_name, sub.sub_name, m.mark
-FROM
-    student s
-        JOIN
-    mark m ON s.student_id = m.student_id
-        JOIN
-    subject sub ON m.sub_id = sub.sub_id
-WHERE
-    sub.sub_name = 'CF';
-
-SELECT 
-    *
-FROM
     student
 WHERE
     student_name LIKE 'h%';
@@ -65,7 +19,7 @@ SELECT
 FROM
     `subject`
 WHERE
-    credit BETWEEN 2 AND 5;
+    credit BETWEEN 3 AND 5;
 
 UPDATE student 
 SET 
@@ -82,66 +36,3 @@ FROM
         JOIN
     `subject` s ON s.sub_id = m.sub_id
 ORDER BY mark DESC , st.student_name;
-
-SELECT 
-    address, COUNT(student_id) AS so_luong_hoc_vien
-FROM
-    student
-GROUP BY address;
-
-SELECT 
-    s.student_id, s.student_name, AVG(mark) AS diem_trung_binh
-FROM
-    student s
-        JOIN
-    mark m ON s.student_id = m.student_id
-GROUP BY s.student_id , s.student_name
-HAVING diem_trung_binh > 15;
-
-SELECT 
-    s.student_id, s.student_name, AVG(m.mark) AS diem_trung_binh
-FROM
-    student s
-        JOIN
-    mark m ON s.student_id = m.student_id
-GROUP BY s.student_id , s.student_name
-HAVING diem_trung_binh > ALL (SELECT 
-        AVG(mark)
-    FROM
-        mark
-    GROUP BY mark.student_id);
-    
-    -- BÀI TẬP
--- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
-SELECT 
-    *
-FROM
-    `subject`
-HAVING credit >= ALL (SELECT 
-        MAX(credit)
-    FROM
-        `subject`);
-        
--- Hiển thị các thông tin môn học có điểm thi lớn nhất.
-SELECT 
-    `subject`.*
-FROM
-    `subject`
-        JOIN
-    mark ON `subject`.sub_id = mark.sub_id
-WHERE
-    mark.mark >= ALL (SELECT 
-            MAX(mark)
-        FROM
-            mark);
-
--- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
-
-SELECT 
-    s.*, AVG(m.mark) AS diem_trung_binh
-FROM
-    student s
-        JOIN
-    mark m ON s.student_id = m.student_id
-GROUP BY s.student_id
-ORDER BY diem_trung_binh DESC;
